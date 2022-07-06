a7a890b3b2010030a5da4c915baf5de3f94dd32c
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-06 10:43:09 -0300

    Firts commit

---
+++ src/test/java/com/jeanbarcellos/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.jeanbarcellos;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
