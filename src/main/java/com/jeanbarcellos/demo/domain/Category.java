6d29be0e7679d16f960a3672b7b2efa55a6d59d6
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-13 16:12:53 -0300

    domain - ajustes

--- src/main/java/com/jeanbarcellos/demo/domain/Category.java
+++ src/main/java/com/jeanbarcellos/demo/domain/Category.java
@@ -2,6 +2,13 @@ package com.jeanbarcellos.demo.domain;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,8 +22,13 @@ import lombok.experimental.Accessors;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "\"category\"")
 public class Category {
 
+    @Id
+    @Type(type = "uuid-char")
+    @Column(name = "id", updatable = false)
     private UUID id;
 
     private String name;
