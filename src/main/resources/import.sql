5f515832386da04ccaeee791e4f3ef4580ec89fb
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-13 16:17:37 -0300

    Configuração do banco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO "category" (id, name, description) VALUES ('824ca30a-a80b-4829-8af5-333b35c177b1', 'Categoria 001', 'Descrição 001');
+INSERT INTO "category" (id, name, description) VALUES ('d61ef95b-ff40-4c25-93d5-5caaae732494', 'Categoria 002', 'Descrição 002');
+INSERT INTO "category" (id, name, description) VALUES ('03324f77-322d-452a-8e84-5ad48c68a85f', 'Categoria 003', 'Descrição 003');
