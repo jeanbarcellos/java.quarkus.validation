e5d635e17ab7f82c66fca5b3bca13a8b1855eba3
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-14 13:58:25 -0300

    Ajustes em PostResource

--- src/main/java/com/jeanbarcellos/demo/resources/PostResource.java
+++ src/main/java/com/jeanbarcellos/demo/resources/PostResource.java
@@ -1,5 +1,11 @@
 package com.jeanbarcellos.demo.resources;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,6 +16,8 @@ import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import com.jeanbarcellos.core.Constants;
+import com.jeanbarcellos.core.exception.ValidationException;
 import com.jeanbarcellos.demo.dtos.PostRequest;
 
 @Path("/posts")
@@ -18,11 +26,25 @@ import com.jeanbarcellos.demo.dtos.PostRequest;
 @Consumes(MediaType.APPLICATION_JSON)
 public class PostResource {
 
+    @Inject
+    Validator validator;
+
     @POST
     public Response insert(@RequestBody PostRequest request) {
         request.validate();
 
         return Response.ok("").build();
+    }
+
+    protected void validate(PostRequest request) {
+        Set<ConstraintViolation<PostRequest>> validateResult = validator.validate(request);
+
+        if (!validateResult.isEmpty()) {
+            throw new ValidationException(
+                    Constants.MSG_ERROR_VALIDATION,
+                    validateResult.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList()));
+        }
+
     }
 
 }
