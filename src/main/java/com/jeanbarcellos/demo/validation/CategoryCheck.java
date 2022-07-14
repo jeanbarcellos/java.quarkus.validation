f2e29795041d7b25d9298df4637983600024ae76
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-14 13:53:37 -0300

    Criação das anotation e Validators customizados

---
+++ src/main/java/com/jeanbarcellos/demo/validation/CategoryCheck.java
@@ -0,0 +1,57 @@
+package com.jeanbarcellos.demo.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.jeanbarcellos.demo.validation.CategoryCheck.List;
+
+/**
+ * Annotation para verificar se o ID de {@code com.jeanbarcellos.demo.domain.Category} é válido.
+ *
+ * <p>
+ * Suporte os seguints tipos:
+ * <ul>
+ * <li>{@code UUID} (Identificador único universal )</li>
+ * </ul>
+ *
+ * @author Jean Silva de Barcellos
+ *
+ */
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = CategoryValidator.class)
+@Repeatable(List.class)
+@Documented
+public @interface CategoryCheck {
+
+    String message() default "{com.jeanbarcellos.demo.validation.CategoryCheck.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Define várias restrições {@code @CategoryCheck} no mesmo elemento.
+     *
+     * @see CategoryCheck
+     */
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+    @Retention(RUNTIME)
+    @Documented
+    public @interface List {
+        CategoryCheck[] value();
+    }
+}
---
+++ src/main/java/com/jeanbarcellos/demo/validation/CategoryValidator.java
@@ -0,0 +1,33 @@
+package com.jeanbarcellos.demo.validation;
+
+import java.util.UUID;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.jeanbarcellos.demo.services.CategoryService;
+
+/**
+ * Validador de Category
+ *
+ * @author Jean Silva de Barcellos
+ *
+ */
+@ApplicationScoped
+public class CategoryValidator implements ConstraintValidator<CategoryCheck, UUID> {
+
+    @Inject
+    private CategoryService service;
+
+    @Override
+    public boolean isValid(UUID value, ConstraintValidatorContext context) {
+        return this.service.exists(value);
+    }
+
+    public boolean isValid(UUID value) {
+        return this.service.exists(value);
+    }
+
+}
