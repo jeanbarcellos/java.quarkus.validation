45877a2be99d404557eb5aeee41906a227b1e128
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-15 16:40:20 -0300

    Implementação de DocsUtils

--- src/main/java/com/jeanbarcellos/core/utils/DocUtils.java
+++ src/main/java/com/jeanbarcellos/core/utils/DocUtils.java
@@ -1,8 +1,18 @@
 package com.jeanbarcellos.core.utils;
 
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocUtils {
+
+    private static final String REGEX_ONLY_NUMBERS = "[\\D]";
+    private static final String STRING_EMPTY = "";
+
+    private static final Integer CPF_LENGHT = 11;
+    private static final Integer CNPJ_LENGHT = 14;
 
     private DocUtils() {
 
@@ -12,17 +22,17 @@ public class DocUtils {
         return generateCPF(false);
     }
 
-    public static String generateCPF(boolean comPontos) {
+    public static String generateCPF(boolean comMascara) {
         int n = 9;
-        int n1 = randomiza(n);
-        int n2 = randomiza(n);
-        int n3 = randomiza(n);
-        int n4 = randomiza(n);
-        int n5 = randomiza(n);
-        int n6 = randomiza(n);
-        int n7 = randomiza(n);
-        int n8 = randomiza(n);
-        int n9 = randomiza(n);
+        int n1 = random(n);
+        int n2 = random(n);
+        int n3 = random(n);
+        int n4 = random(n);
+        int n5 = random(n);
+        int n6 = random(n);
+        int n7 = random(n);
+        int n8 = random(n);
+        int n9 = random(n);
         int d1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10;
 
         d1 = 11 - (mod(d1, 11));
@@ -34,74 +44,23 @@ public class DocUtils {
 
         d2 = 11 - (mod(d2, 11));
 
-        String retorno = "";
-
         if (d2 >= 10)
             d2 = 0;
 
-        if (comPontos)
-            retorno = "" + n1 + n2 + n3 + '.' + n4 + n5 + n6 + '.' + n7 + n8 + n9 + '-' + d1 + d2;
-        else
-            retorno = "" + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + d1 + d2;
+        var resultado = STRING_EMPTY + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + d1 + d2;
 
-        return retorno;
-    }
-
-    public static String generateCNPJ() {
-        return generateCNPJ(false);
-    }
-
-    public static String generateCNPJ(boolean comPontos) {
-        int n = 9;
-        int n1 = randomiza(n);
-        int n2 = randomiza(n);
-        int n3 = randomiza(n);
-        int n4 = randomiza(n);
-        int n5 = randomiza(n);
-        int n6 = randomiza(n);
-        int n7 = randomiza(n);
-        int n8 = randomiza(n);
-        int n9 = 0;
-        int n10 = 0;
-        int n11 = 0;
-        int n12 = 1;
-        int d1 = n12 * 2 + n11 * 3 + n10 * 4 + n9 * 5 + n8 * 6 + n7 * 7 + n6 * 8 + n5 * 9 + n4 * 2 + n3 * 3 + n2 * 4
-                + n1 * 5;
-
-        d1 = 11 - (mod(d1, 11));
-
-        if (d1 >= 10)
-            d1 = 0;
-
-        int d2 = d1 * 2 + n12 * 3 + n11 * 4 + n10 * 5 + n9 * 6 + n8 * 7 + n7 * 8 + n6 * 9 + n5 * 2 + n4 * 3 + n3 * 4
-                + n2 * 5 + n1 * 6;
-
-        d2 = 11 - (mod(d2, 11));
-
-        if (d2 >= 10)
-            d2 = 0;
-
-        String retorno = null;
-
-        if (comPontos)
-            retorno = "" + n1 + n2 + "." + n3 + n4 + n5 + "." + n6 + n7 + n8 + "/" + n9 + n10 + n11 + n12 + "-" + d1
-                    + d2;
-        else
-            retorno = "" + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + d1 + d2;
-
-        return retorno;
+        return comMascara ? formatCPF(resultado) : resultado;
     }
 
     public static boolean isCPF(String cpf) {
 
         cpf = removeSpecialChars(cpf);
 
-        // considera-se erro cpf's formados por uma sequencia de numeros iguais
-        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222")
-                || cpf.equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555")
-                || cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888")
-                || cpf.equals("99999999999") || (cpf.length() != 11))
-            return (false);
+        var numerosInvalidos = Arrays.asList("00000000000", "11111111111", "22222222222", "33333333333", "44444444444",
+                "55555555555", "66666666666", "77777777777", "88888888888", "99999999999");
+
+        if (numerosInvalidos.contains(cpf) || (cpf.length() != CPF_LENGHT))
+            return false;
 
         char dig10, dig11;
         int sm, i, r, num, peso;
@@ -151,15 +110,67 @@ public class DocUtils {
         }
     }
 
+    public static String formatCPF(String cpf) {
+        cpf = String.format("%011d", new BigInteger(cpf));
+
+        Pattern pattern = Pattern.compile("(\\d{3})(\\d{3})(\\d{3})(\\d{2})");
+        Matcher matcher = pattern.matcher(cpf);
+
+        if (matcher.matches())
+            cpf = matcher.replaceAll("$1.$2.$3-$4");
+
+        return cpf;
+    }
+
+    public static String generateCNPJ() {
+        return generateCNPJ(false);
+    }
+
+    public static String generateCNPJ(boolean comMascara) {
+        int n = 9;
+        int n1 = random(n);
+        int n2 = random(n);
+        int n3 = random(n);
+        int n4 = random(n);
+        int n5 = random(n);
+        int n6 = random(n);
+        int n7 = random(n);
+        int n8 = random(n);
+        int n9 = 0;
+        int n10 = 0;
+        int n11 = 0;
+        int n12 = 1;
+        int d1 = n12 * 2 + n11 * 3 + n10 * 4 + n9 * 5 + n8 * 6 + n7 * 7 + n6 * 8 + n5 * 9 + n4 * 2 + n3 * 3 + n2 * 4
+                + n1 * 5;
+
+        d1 = 11 - (mod(d1, 11));
+
+        if (d1 >= 10)
+            d1 = 0;
+
+        int d2 = d1 * 2 + n12 * 3 + n11 * 4 + n10 * 5 + n9 * 6 + n8 * 7 + n7 * 8 + n6 * 9 + n5 * 2 + n4 * 3 + n3 * 4
+                + n2 * 5 + n1 * 6;
+
+        d2 = 11 - (mod(d2, 11));
+
+        if (d2 >= 10)
+            d2 = 0;
+
+        var resultado = STRING_EMPTY + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + d1 + d2;
+
+        return comMascara ? formatCNPJ(resultado) : resultado;
+    }
+
     public static boolean isCNPJ(String cnpj) {
 
         cnpj = removeSpecialChars(cnpj);
 
+        var numerosInvalidos = Arrays.asList("00000000000000", "11111111111111", "22222222222222", "33333333333333",
+                "44444444444444", "55555555555555", "66666666666666", "77777777777777", "88888888888888",
+                "99999999999999");
+
         // considera-se erro cnpj's formados por uma sequencia de numeros iguais
-        if (cnpj.equals("00000000000000") || cnpj.equals("11111111111111") || cnpj.equals("22222222222222")
-                || cnpj.equals("33333333333333") || cnpj.equals("44444444444444") || cnpj.equals("55555555555555")
-                || cnpj.equals("66666666666666") || cnpj.equals("77777777777777") || cnpj.equals("88888888888888")
-                || cnpj.equals("99999999999999") || (cnpj.length() != 14))
+        if (numerosInvalidos.contains(cnpj) || (cnpj.length() != CNPJ_LENGHT))
             return (false);
 
         char dig13, dig14;
@@ -214,22 +225,38 @@ public class DocUtils {
         }
     }
 
-    private static String removeSpecialChars(String doc) {
-        if (doc.contains(".")) {
-            doc = doc.replace(".", "");
-        }
-        if (doc.contains("-")) {
-            doc = doc.replace("-", "");
-        }
-        if (doc.contains("/")) {
-            doc = doc.replace("/", "");
-        }
-        return doc;
+    public static String formatCNPJ(String cnpj) {
+        cnpj = String.format("%014d", new BigInteger(cnpj));
+
+        Pattern pattern = Pattern.compile("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})");
+        Matcher matcher = pattern.matcher(cnpj);
+
+        if (matcher.matches())
+            cnpj = matcher.replaceAll("$1.$2.$3/$4-$5");
+
+        return cnpj;
     }
 
-    private static int randomiza(int n) {
-        int ranNum = (int) (Math.random() * n);
-        return ranNum;
+    public static boolean isCPForCNPJ(String number) {
+        number = removeSpecialChars(number);
+
+        if (number.length() == CPF_LENGHT) {
+            return isCPF(number);
+        }
+
+        if (number.length() == CNPJ_LENGHT) {
+            return isCNPJ(number);
+        }
+
+        return false;
+    }
+
+    private static String removeSpecialChars(String doc) {
+        return doc.replaceAll(REGEX_ONLY_NUMBERS, STRING_EMPTY);
+    }
+
+    private static int random(int n) {
+        return (int) (Math.random() * n);
     }
 
     private static int mod(int dividendo, int divisor) {
--- src/test/java/com/jeanbarcellos/core/utils/DocUtilsTest.java
+++ src/test/java/com/jeanbarcellos/core/utils/DocUtilsTest.java
@@ -1,5 +1,6 @@
 package com.jeanbarcellos.core.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -57,10 +58,172 @@ class DocUtilsTest {
     void generateCPF_entryNoArgs_shouldGenerateCpfValid() {
 
         // Arrange && Act
+        var result = DocUtils.generateCPF();
+
+        // assert
+        assertNotNull(result);
+        assertEquals(11, result.length());
+    }
+
+    @Test
+    void generateCPF_entryArgTrue_shouldGenerateCpfValidAndFormated() {
+
+        // Arrange && Act
+        boolean comMascara = true;
+        var result = DocUtils.generateCPF(comMascara);
+
+        // assert
+        assertNotNull(result);
+        assertEquals(14, result.length());
+    }
+
+    @Test
+    void formatCPF_inputString11Chars_shouldReturnStringFromated() {
+
+        // Arrange
+        String cpf = "00123456789";
+        String cpfFormated = "001.234.567-89";
+
+        // Arrange && Act
+        String result = DocUtils.formatCPF(cpf);
+
+        // assert
+        assertEquals(cpfFormated, result);
+    }
+
+    @Test
+    void formatCPF_inputString3Chars_shouldReturnStringFromated() {
+
+        // Arrange
+        String cpf = "189";
+        String cpfFormated = "000.000.001-89";
+
+        // Arrange && Act
+        String result = DocUtils.formatCPF(cpf);
+
+        // assert
+        assertEquals(cpfFormated, result);
+    }
+
+    // ****
+
+    @Test
+    void isCNPJ_entryCnpjInvalid_shouldReturnFalse() {
+
+        // Arrange
+        var cpf = "12345678900";
+
+        // Act
+        var result = DocUtils.isCNPJ(cpf);
+
+        // assert
+        assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "00000000000000", "11111111111111", "22222222222222", "33333333333333", "44444444444444", "55555555555555",
+            "66666666666666", "77777777777777", "88888888888888", "99999999999999", "1234567890134", "ASDASDASDASASD",
+            "123ASD123AS13S"
+    })
+    void isCNPJ_entryCnpjInvalids_shouldReturnFalse(String cnpj) {
+
+        // Arrange && Act
+        var result = DocUtils.isCNPJ(cnpj);
+
+        // assert
+        assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "02804097000110", "46313752000103", "46552618000165", "06559405000140"
+    })
+    void isCNPJ_entryCnpjValid_shouldReturnTrue(String cnpj) {
+
+        // Arrange && Act
+        var result = DocUtils.isCNPJ(cnpj);
+
+        // assert
+        assertTrue(result);
+    }
+
+    @Test
+    void generateCNPJ_entryNoArgs_shouldGenerateCnpjValid() {
+
+        // Arrange && Act
         var result = DocUtils.generateCNPJ();
 
         // assert
         assertNotNull(result);
+        assertEquals(14, result.length());
     }
 
+    @Test
+    void generateCNPJ_entryArgTrue_shouldGenerateCnpjValidAndFormated() {
+
+        // Arrange && Act
+        boolean comMascara = true;
+        var result = DocUtils.generateCNPJ(comMascara);
+
+        // assert
+        assertNotNull(result);
+        assertEquals(18, result.length());
+    }
+
+    @Test
+    void formatCNPJ_inputString14Chars_shouldReturnStringFromated() {
+
+        // Arrange
+        String cnpj = "2804097000110";
+        String cnpjFormated = "02.804.097/0001-10";
+
+        // Arrange && Act
+        String result = DocUtils.formatCNPJ(cnpj);
+
+        // assert
+        assertEquals(cnpjFormated, result);
+    }
+
+    @Test
+    void formatCNPJ_inputString18Chars_shouldReturnStringFromated() {
+
+        // Arrange
+        String cnpj = "191";
+        String cnpjFormated = "00.000.000/0001-91";
+
+        // Arrange && Act
+        String result = DocUtils.formatCNPJ(cnpj);
+
+        // assert
+        assertEquals(cnpjFormated, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "49118537006", "57565900052", "01531188060", "96035245498",
+            "02804097000110", "46313752000103", "46552618000165", "06559405000140"
+    })
+    void isCPForCNPJ_inputStringValids_returnTrue(String numeros) {
+
+        // Arrange && Act
+        var result = DocUtils.isCPForCNPJ(numeros);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "77777777", "88888888888", "99999999999", "1234567891", "ASDASDASDAS", "123ASD123AS",
+            "66666666666666", "77777777777777", "88888888888888", "99999999999999", "1234567890134", "ASDASDASDASASD"
+    })
+    void isCPForCNPJ_inputStringinvalids_returnFalse(String numeros) {
+
+        // Arrange && Act
+        var result = DocUtils.isCPForCNPJ(numeros);
+
+        // Assert
+        assertFalse(result);
+    }
 }
