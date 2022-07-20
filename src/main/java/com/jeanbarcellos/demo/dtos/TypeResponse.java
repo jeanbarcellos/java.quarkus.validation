0e36d93dd867675067cb856c92dd06e424470c72
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-19 23:28:57 -0300

    Criação de TypeResource para obtenção dos tipos

--- src/main/java/com/jeanbarcellos/demo/dtos/TypeResponse.java
+++ src/main/java/com/jeanbarcellos/demo/dtos/TypeResponse.java
@@ -1,5 +1,10 @@
 package com.jeanbarcellos.demo.dtos;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.jeanbarcellos.demo.domain.Type;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,5 +22,17 @@ public class TypeResponse {
 
     private Integer id;
     private String name;
+
+    public static TypeResponse of(Type type) {
+        return TypeResponse
+                .builder()
+                .id(type.getId())
+                .name(type.getName())
+                .build();
+    }
+
+    public static List<TypeResponse> of(List<Type> types) {
+        return types.stream().map(TypeResponse::of).collect(Collectors.toList());
+    }
 
 }
---
+++ src/main/java/com/jeanbarcellos/demo/resources/TypeResource.java
@@ -0,0 +1,51 @@
+package com.jeanbarcellos.demo.resources;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import com.jeanbarcellos.demo.dtos.ExistsResponse;
+import com.jeanbarcellos.demo.services.TypeService;
+
+@Path("/types")
+@Tag(name = "Manutenção de Tipos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RequestScoped
+public class TypeResource {
+
+    @Inject
+    private TypeService service;
+
+    @GET
+    public Response list() {
+        return Response.ok(this.service.list()).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response exists(@PathParam("id") Integer id) {
+        return Response.ok(ExistsResponse.of(this.service.exists(id))).build();
+    }
+
+    @GET
+    @Path("/visibility")
+    public Response visibility() {
+        return Response.ok(this.service.visibility()).build();
+    }
+
+    @GET
+    @Path("/people")
+    public Response people() {
+        return Response.ok(this.service.people()).build();
+    }
+
+}
