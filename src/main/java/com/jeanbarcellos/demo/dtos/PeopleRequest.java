4dd43643e47337168402769accb84f9269d37412
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-07-29 20:47:51 -0300

    Ajustes nas mensagens de erro personalizadas

--- src/main/java/com/jeanbarcellos/demo/dtos/PeopleRequest.java
+++ src/main/java/com/jeanbarcellos/demo/dtos/PeopleRequest.java
@@ -28,32 +28,21 @@ import lombok.experimental.Accessors;
 @Builder
 public class PeopleRequest extends ValidationRequestBase {
 
-    public static final String MSG_ERROR_NAME_NOT_NULL_OR_EMPTY = "O campo 'name' não deve ser nulo ou estar vazio";
-    public static final String MSG_ERROR_NAME_SIZE = "O campo 'name' deve possuir tamanho entre 4 e 128";
-
-    public static final String MSG_ERROR_EMAIL_NOT_NULL_OR_EMPTY = "O campo 'email' não deve ser nulo ou estar vazio";
-    public static final String MSG_ERROR_EMAIL_INVALID = "O 'email' informado é inválido";
-
-    public static final String MSG_ERROR_CPF_NOT_NULL_OR_EMPTY = "O campo 'identificationNumber' não deve ser nulo ou estar vazio";
-    public static final String MSG_ERROR_CPF_INVALID = "O 'CPF' informado é inválido";
-
-    public static final String MSG_ERROR_VISIBILITY_INVALID = "O campo 'type' possui valor inválido";
-
     @JsonIgnore
     private UUID id;
 
-    @NotEmpty(message = MSG_ERROR_NAME_NOT_NULL_OR_EMPTY)
-    @Size(min = 4, max = 128, message = MSG_ERROR_NAME_SIZE)
+    @NotEmpty(message = "{PeopleRequest.name.notNullOrEmpty}")
+    @Size(min = 4, max = 128, message = "{PeopleRequest.name.size}")
     private String name;
 
-    @NotEmpty(message = MSG_ERROR_EMAIL_NOT_NULL_OR_EMPTY)
-    @Email(message = MSG_ERROR_EMAIL_INVALID)
+    @NotEmpty(message = "{PeopleRequest.email.notNullOrEmpty}")
+    @Email(message = "{PeopleRequest.email.invalid}")
     private String email;
 
-    @NotEmpty(message = MSG_ERROR_CPF_NOT_NULL_OR_EMPTY)
-    @CPF(message = MSG_ERROR_CPF_INVALID)
+    @NotEmpty(message = "{PeopleRequest.cpf.notNullOrEmpty}")
+    @CPF(message = "{PeopleRequest.cpf.invalid}")
     private String identificationNumber;
 
-    @TypeCheck(type = Types.PEOPLE, message = MSG_ERROR_VISIBILITY_INVALID)
+    @TypeCheck(type = Types.PEOPLE, message = "{PeopleRequest.visibility.invalid}")
     private Integer type;
 }
